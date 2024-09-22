bd319b25d9335c33d31bbadbed1a67877cd0cbaf
Lina Marcela Landys Samboni <lina_landys@MacBook-Pro-de-Lina.local>
2024-09-22 11:53:34 -0500

    Add folders, base files for landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 //import { useState } from 'react'
+import LandingPage from './screens/landing/landing'
 import './App.css'
 
 function App() {
 
   return (
     <>
-    
+    <LandingPage/>
     </>
   )
 }
---
+++ src/screens/landing/landing.tsx
@@ -0,0 +1,13 @@
+//import { useState } from 'react'
+import './landing.css'
+
+function LandingPage() {
+
+  return (
+    <>
+    hola
+    </>
+  )
+}
+
+export default LandingPage
