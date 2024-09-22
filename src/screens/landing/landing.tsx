91f2ebc90b429a393709bff19661b32041d7ca3a
Lina Marcela Landys Samboni <lina_landys@MacBook-Pro-de-Lina.local>
2024-09-22 18:25:25 -0500

    FIX css

--- src/screens/landing/landing.tsx
+++ src/screens/landing/landing.tsx
@@ -1,4 +1,4 @@
-//import { useState } from 'react'
+// import { useState } from 'react'
 import NavBar from "../../components/navBar/navBar";
 import LanCards from '../../components/cardsType/landingCards'
 
@@ -6,28 +6,31 @@ import "./landing.css";
 
 function LandingPage() {
   return (
-    <>
+    <div className="landing-page">
       <section>
         <div className="container-1">
            <NavBar />
           <div className="texts-div">
-           
-          <h1>Make the difference</h1>
-          <p className="paragraph">
-            You can adopt pets, report lost or stray animals, access veterinary
-            services, and shop for pet care products. Find your new friend, help
-            reunite pets with their owners, schedule veterinary appointments and
-            discover offers in our store.
-          </p>
-          <p className="bold-text">Join us and make a difference today!</p>
+            <h1>Make the difference</h1>
+            <p className="paragraph">
+              You can adopt pets, report lost or stray animals, access veterinary
+              services, and shop for pet care products. Find your new friend, help
+              reunite pets with their owners, schedule veterinary appointments and
+              discover offers in our store.
+            </p>
+            <p className="bold-text">Join us and make a difference today!</p>
           </div>
         </div>
 
-        <img className="dog-img" src="./src/img/big-dog.png"></img>
+        <img className="dog-img" src="./src/img/big-dog.png" />
+        <img className="crown-img" src="./src/img/Crown.png" />
+        <img className="stars-img" src="./src/img/stars.png" />
       </section>
 
-      <LanCards/>
-    </>
+<div className="cards-111">
+      <LanCards /> 
+      </div>
+    </div>
   );
 }
 
