6ef72993cd9c56ada13d77ef60ed92154b583025
Lina Marcela Landys Samboni <lina_landys@MacBook-Pro-de-Lina.local>
2024-10-03 09:46:28 -0500

    upt data

--- src/services/pets.tsx
+++ src/services/pets.tsx
@@ -1,115 +1,151 @@
 export const dogs = [
     {
-        name: "Buddy",
+        name: "Draco",
         size: "Large",
         age: 4,
-        image: "https://example.com/dog1.jpg"
+        image: "./src/img/p9.jpeg"
     },
     {
-        name: "Max",
+        name: "Toby",
         size: "Medium",
         age: 2,
-        image: "https://example.com/dog2.jpg"
+        image: "./src/img/p8.jpeg"
     },
     {
-        name: "Bella",
+        name: "Perro",
         size: "Small",
         age: 3,
-        image: "https://example.com/dog3.jpg"
+        image: "./src/img/p7.jpeg"
     },
     {
-        name: "Buddy",
+        name: "Luna",
         size: "Large",
         age: 4,
-        image: "https://example.com/dog1.jpg"
+        image: "./src/img/p6.jpeg"
     },
     {
-        name: "Max",
+        name: "Mango",
         size: "Medium",
         age: 2,
-        image: "https://example.com/dog2.jpg"
+        image: "./src/img/p5.jpeg"
     },
     {
-        name: "Bella",
+        name: "Stinky",
         size: "Small",
         age: 3,
-        image: "https://example.com/dog3.jpg"
+        image: "./src/img/p4.jpeg"
     },
     {
-        name: "Buddy",
+        name: "Mimi",
         size: "Large",
         age: 4,
-        image: "https://example.com/dog1.jpg"
+        image: "./src/img/p3.jpeg"
     },
     {
-        name: "Max",
+        name: "Yuji",
         size: "Medium",
         age: 2,
-        image: "https://example.com/dog2.jpg"
+        image: "./src/img/p2.jpeg"
     },
     {
-        name: "Bella",
+        name: "Megumi",
         size: "Small",
         age: 3,
-        image: "https://example.com/dog3.jpg"
+        image: "./src/img/p1.jpeg"
+    },
+    {
+        name: "Nobara",
+        size: "Small",
+        age: 3,
+        image: "./src/img/p10.jpeg"
+    },
+    {
+        name: "Ciel",
+        size: "Small",
+        age: 3,
+        image: "./src/img/p11.jpeg"
+    },
+    {
+        name: "Edward",
+        size: "Small",
+        age: 3,
+        image: "./src/img/p12.jpeg"
     }
 ];
 
 
 const cats = [
     {
-        name: "Luna",
+        name: "Dave the magical cheese wizard",
         size: "Small",
-        age: 1,
-        image: "https://example.com/cat1.jpg"
+        age: "9 years old",
+        image: "./src/img/g1.webp"
+    },
+    {
+        name: "Viktor",
+        size: "Medium",
+        age: "5 years old",
+        image: "./src/img/g2.jepg"
+    },
+    {
+        name: "Sebastian",
+        size: "Large",
+        age: "7 years old",
+        image: "./src/img/g3.jepg"
+    },
+    {
+        name: "Penny",
+        size: "Small",
+        age: "4 years old",
+        image: "./src/img/g4.avif"
     },
     {
         name: "Oliver",
         size: "Medium",
         age: 5,
-        image: "https://example.com/cat2.jpg"
+        image: "./src/img/g5.webp"
     },
     {
         name: "Milo",
         size: "Large",
-        age: 3,
-        image: "https://example.com/cat3.jpg"
+        age: "2 years old",
+        image: "./src/img/g6.jepg"
     },
     {
         name: "Luna",
         size: "Small",
-        age: 1,
-        image: "https://example.com/cat1.jpg"
+        age: "1 year old",
+        image: "./src/img/g7.webp"
     },
     {
-        name: "Oliver",
+        name: "Alan",
         size: "Medium",
-        age: 5,
-        image: "https://example.com/cat2.jpg"
+        age: "5 years old",
+        image: "./src/img/g8.webp"
     },
     {
-        name: "Milo",
+        name: "Gojo",
         size: "Large",
-        age: 3,
-        image: "https://example.com/cat3.jpg"
+        age: "6 years old",
+        image: "./src/img/g9.jepg"
     },
     {
-        name: "Luna",
-        size: "Small",
-        age: 1,
-        image: "https://example.com/cat1.jpg"
-    },
-    {
-        name: "Oliver",
-        size: "Medium",
-        age: 5,
-        image: "https://example.com/cat2.jpg"
-    },
-    {
-        name: "Milo",
+        name: "Geto",
         size: "Large",
-        age: 3,
-        image: "https://example.com/cat3.jpg"
+        age: "3 years old",
+        image: "./src/img/g10.jepg"
+    },
+    {
+        name: "Taison",
+        size: "Large",
+        age: "6 years old",
+        image: "./src/img/g11.jepg"
+    },
+    {
+        name: "Suriel",
+        size: "Large",
+        age: "2 years old",
+        image: "./src/img/g12.jepg"
     }
 ];
 export default cats
