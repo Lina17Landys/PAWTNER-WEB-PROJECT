70a611f7202c2cf757dd351c98dbbfe30582151d
Manuelard03 <manuela.rdavila03@gmail.com>
2024-11-04 16:08:25 -0500

    FIX/DiseaseForum and Navbar: CSS

--- src/components/BarDashboard/nav-dash.tsx
+++ src/components/BarDashboard/nav-dash.tsx
@@ -4,10 +4,8 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 import { Link } from 'react-router-dom';
 
 const BarDash: React.FC = () => {
-  // State to toggle menu visibility
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  // Function to toggle menu
   const toggleMenu = () => {
     setIsMenuOpen((prevState) => !prevState);
   };
@@ -17,12 +15,10 @@ const BarDash: React.FC = () => {
       <div className="navbar-left">
         <img className="logo-img2" src="./src/img/logo.png" alt="Logo" />
         
-        {/* Hamburger Menu */}
         <div className="hamburger-menu" onClick={toggleMenu}>
           <i className="fa fa-bars"></i>
         </div>
         
-        {/* Navbar Menu - Visible/Hidden based on state */}
         <ul className={`navbar-menu ${isMenuOpen ? 'active' : ''}`}>
           <li><Link to="/Dashboard">Home</Link></li>
           <li><Link to="/pets">Pets</Link></li>
--- src/components/DiseaseForum/CommentSection/CommentSection.tsx
+++ src/components/DiseaseForum/CommentSection/CommentSection.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Comment } from '../../../types/postTypes';
 import { useAuthUser } from '../../../hooks/useAuthUser';
+import './CommentSection.css';
 
 interface CommentSectionProps {
   comments: Comment[];
@@ -31,23 +32,27 @@ const CommentSection: React.FC<CommentSectionProps> = ({ comments, onAddComment,
       <div className="comment-input">
         <input
           type="text"
-          placeholder="Escribe un comentario..."
+          placeholder="Add comment..."
           value={newCommentText}
           onChange={(e) => setNewCommentText(e.target.value)}
         />
-        <button onClick={handleAddComment}>Comentar</button>
+        <button className='Submit-btn' onClick={handleAddComment}>Submit</button>
       </div>
-
+      <hr className='divisor'/>
       <div className="comments-list">
+        <h2 className="comments-title">Comments</h2>
         {comments.map((comment) => (
           <div key={comment.id} className="comment">
-            <strong>{comment.username}:</strong> {comment.text}
+            <strong className="comment-username">{comment.username}:</strong> {comment.text}
             {authUser?.uid === comment.userId && (
-              <button onClick={() => onDeleteComment(comment.id)}>Eliminar</button>
-            )}
-          </div>
-        ))}
-      </div>
+              <button className="Delete-btn" onClick={() => onDeleteComment(comment.id)}>
+                <svg  xmlns="http://www.w3.org/2000/svg"  width="18"  height="18"  viewBox="0 0 24 24"  fill="none"  stroke="#E21E27"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-trash"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 7l16 0" /><path d="M10 11l0 6" /><path d="M14 11l0 6" /><path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" /><path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" /></svg>
+              </button>
+      )}
+    </div>
+  ))}
+</div>
+
     </div>
   );
 };
