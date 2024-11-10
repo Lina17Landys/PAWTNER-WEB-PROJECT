b3c7350094880830deaa4ae2e4d3c7e036777c25
Manuelard03 <manuela.rdavila03@gmail.com>
2024-11-09 21:34:08 -0500

    FIX/Landing Page: Structure and CSS

--- src/components/Landing/PetsSection/PetsSection.tsx
+++ src/components/Landing/PetsSection/PetsSection.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
+import './PetsSection.css'
 
 const pets = [
-  { name: 'Hunter', imgSrc: '/path/to/hunter.png', alt: 'Hunter the dog' },
-  { name: 'Tonio', imgSrc: '/path/to/tonio.png', alt: 'Tonio the cat' },
-  { name: 'Apolo', imgSrc: '/path/to/apolo.png', alt: 'Apolo the parrot' },
+  { name: 'Hunter', imgSrc: 'src/assets/images/LandingImg/Cards-Pets/c1.png', alt: 'Hunter the dog' },
+  { name: 'Tonio', imgSrc: 'src/assets/images/LandingImg/Cards-Pets/c2.png', alt: 'Tonio the dog' },
+  { name: 'Apolo', imgSrc: 'src/assets/images/LandingImg/Cards-Pets/c3.png', alt: 'Apolo the cat' },
 ];
 
 const PetsSection: React.FC = () => {
   return (
-    <section id="pets" aria-labelledby="pets-heading">
+    <section className="pets-section" aria-labelledby="pets-heading">
       <h2 id="pets-heading">Meet a New Friend!</h2>
       <div className="pet-list">
         {pets.map((pet) => (
           <article key={pet.name} className="pet-card">
-            <img src={pet.imgSrc} alt={pet.alt} />
+            <img className='pets-section-img' src={pet.imgSrc} alt={pet.alt} />
             <h3>{pet.name}</h3>
           </article>
         ))}
