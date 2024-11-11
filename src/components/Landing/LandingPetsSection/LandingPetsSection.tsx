6ddee8eae75d0025878880164fb08e4ce73568a4
Manuelard03 <manuela.rdavila03@gmail.com>
2024-11-11 08:16:35 -0500

    FIX/Landing Page: PetsSection CSS

--- src/components/Landing/LandingPetsSection/LandingPetsSection.tsx
+++ src/components/Landing/LandingPetsSection/LandingPetsSection.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import LandingPetsCard from '../LandingPetsCard/LandingPetsCard';
 import './LandingPetsSection.css';
+import ShootingStarLanding from '../../../assets/Illustrations-details/star2.png'
 
 const petsData = [
   { name: 'Hunter', image: 'src/assets/images/LandingImg/Cards-Pets/PetsCard-1.png' },
@@ -11,7 +12,10 @@ const petsData = [
 const LandingPetsSection: React.FC = () => {
   return (
     <section className="landing-pets-section">
-      <h2 className='landing-pets-title-1'>Meet a</h2>
+      <div className='landing-pets-titleSection'>
+        <h2 className='landing-pets-title-1'>Meet a</h2>
+        <img className='landing-shootingStar' src={ShootingStarLanding} alt="Shooting Star illustration" />
+      </div>
       <h2 className='landing-pets-title-2'>New Friend</h2>
       <div className="landing-pets-cards-container">
         {petsData.map((pet, index) => (
