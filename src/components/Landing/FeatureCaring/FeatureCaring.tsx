ccbd6930056b306966d22feea54a3c03f1f31a8f
Manuelard03 <manuela.rdavila03@gmail.com>
2024-11-12 11:11:13 -0500

    ADD/Landing Page: Footer structure and CSS

--- src/components/Landing/FeatureCaring/FeatureCaring.tsx
+++ src/components/Landing/FeatureCaring/FeatureCaring.tsx
@@ -8,7 +8,7 @@ const FeatureCaring: React.FC = () => {
       <div className='feature-caring-container'>
       <img className='Caring-Dog-img' src={CaringDog} alt="Sick dog" />
         <div className='feature-caring-description'>
-          <h2 className="feature-caring-heading">Report</h2>
+          <h2 className="feature-caring-heading">Caring</h2>
           <article>
             <p className='feature-caring-text'>
             Pawtner is dedicated to making a meaningful difference in the lives of pets and their families. We connect animals in need with loving homes, provide essential resources for their care and well-being, and offer easy access to veterinary services. Our goal is to create a supportive community that promotes adoption, rescue, and responsible pet ownership.</p>
--- src/components/Landing/Footer/Footer.tsx
+++ src/components/Landing/Footer/Footer.tsx
@@ -1,19 +1,30 @@
 import React from 'react';
+import './Footer.css';
+import Logo from '../../../assets/Logo/logo.png';
+import InstagramLogo from '../../../assets/Icons/SocialMediaIcons/Instagram - Logo.png';
+import FacebookLogo from '../../../assets/Icons/SocialMediaIcons/Facebook - Logo.png';
+import YoutubeLogo from '../../../assets/Icons/SocialMediaIcons/Youtube - Logo.png';
 
 const Footer: React.FC = () => {
   return (
     <footer>
-      <div>
-        <p>Pawtner &copy; {new Date().getFullYear()}</p>
-        <ul>
+      <div className='footer-section'>
+        <div className='footer-pawtner-brand-section'>
+          <img className='footer-pawtner-logo' src= {Logo} alt="Pawtner Logo" />
+          <p>Pawtner &copy; {new Date().getFullYear()}</p>
+        </div>
+        <ul className='footer-terms-section'>
           <li><a href="/terms">Terms of Service</a></li>
           <li><a href="/privacy">Privacy Policy</a></li>
         </ul>
       </div>
-      <div>
-        <a href="https://facebook.com" aria-label="Facebook"><img src="/icons/facebook.svg" alt="Facebook" /></a>
-        <a href="https://instagram.com" aria-label="Instagram"><img src="/icons/instagram.svg" alt="Instagram" /></a>
-        <a href="https://twitter.com" aria-label="Twitter"><img src="/icons/twitter.svg" alt="Twitter" /></a>
+      <div className='footer-right-section'>
+        <p>A comprehensive platform for pet adoption, health forums, and pet care resources.</p>
+        <div className='footer-socialmedia-section '>
+          <a href="https://facebook.com" aria-label="Facebook"><img src={FacebookLogo} alt="Facebook" /></a>
+          <a href="https://instagram.com" aria-label="Instagram"><img src={InstagramLogo} alt="Instagram" /></a>
+          <a href="https://www.youtube.com/" aria-label="Twitter"><img src={YoutubeLogo} alt="Twitter" /></a>
+        </div>
       </div>
     </footer>
   );
